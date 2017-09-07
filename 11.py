ae18f300ebe82b7a4b3478d80210931c2a780166
zzd <404251272@qq.com>
2017-09-07 16:59:49 +0800

    ceshi

---
+++ 11.py
@@ -0,0 +1,12 @@
+def getdata():
+	def _data(self, evadata=None):
+		return 2+3
+	print "66666666"
+	
+	evadata = None
+	a = _data(evadata)
+	print a
+	return a
+	
+if __name__ == '__main__':
+	getdata()
